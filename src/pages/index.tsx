a3b10127fb84e2282f8f56f0e3c08292fe466a83
weber souza || dev-CMS <61756012+g4tsbycms@users.noreply.github.com>
2023-07-06 23:33:38 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react'
 import Questionario from '../../components/Questionario'
 import { useRouter } from 'next/router'
 
-
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://quiz-jomap-cuminancia-2023.vercel.app'
+//const BASE_URL = 'http://localhost:3000/api'
 
 export default function Home() {
   const router = useRouter()
